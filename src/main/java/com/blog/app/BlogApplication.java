a77db8050c76d9ca9d1d39234d8f85c33a6ac629
Athirson <athirsonarceus@gmail.com>
2023-05-25 19:32:45 -0300

    bah

--- src/main/java/com/blog/app/BlogApplication.java
+++ src/main/java/com/blog/app/BlogApplication.java
@@ -1,8 +1,11 @@
 package com.blog.app;
 
-import com.blog.app.entities.Role;
-import com.blog.app.repositories.RoleRepository;
-import com.blog.app.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.blog.app.config.BootstrapData;
+
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.extensions.Extension;
@@ -10,80 +13,25 @@ import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Spring Boot 3 Blog API",
-				version = "1.0",
-				description = "Spring Boot 3 Blog API REST APIs Documentation",
-				contact = @Contact(
-						name = "Athirson",
-						url = "https://athirsonsilva.github.io/personal-portfolio/",
-						email = "athirsonarceus@gmail.com"
-				),
-				license = @License(
-						name = "Apache 2.0",
-						url = "https://www.apache.org/licenses/LICENSE-2.0.html"
-				),
-				termsOfService = "https://swagger.io/terms/",
-				extensions = {
-						@Extension(name = "x-logo", properties = {
-								@ExtensionProperty(name = "url", value = "https://athirsonsilva.github.io/personal-portfolio/assets/img/logo.png"),
-								@ExtensionProperty(name = "altText", value = "Spring Boot 3 Blog API")
-						})
-				}
-		),
-		externalDocs = @ExternalDocumentation(
-				description = "Spring Boot 3 Blog API REST APIs Documentation",
-				url = "https://athirsonsilva.github.io/personal-portfolio/"
-		)
-)
+@OpenAPIDefinition(info = @Info(title = "Spring Boot 3 Blog API", version = "1.0", description = "Spring Boot 3 Blog API REST APIs Documentation", contact = @Contact(name = "Athirson", url = "https://athirsonsilva.github.io/personal-portfolio/", email = "athirsonarceus@gmail.com"), license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"), termsOfService = "https://swagger.io/terms/", extensions = {
+		@Extension(name = "x-logo", properties = {
+				@ExtensionProperty(name = "url", value = "https://athirsonsilva.github.io/personal-portfolio/assets/img/logo.png"),
+				@ExtensionProperty(name = "altText", value = "Spring Boot 3 Blog API")
+		})
+}), externalDocs = @ExternalDocumentation(description = "Spring Boot 3 Blog API REST APIs Documentation", url = "https://athirsonsilva.github.io/personal-portfolio/"))
+@RequiredArgsConstructor
 public class BlogApplication implements CommandLineRunner {
-	@Autowired
-	private final PasswordEncoder passwordEncoder;
-	@Autowired
-	private final UserRepository userRepository;
-	@Autowired
-	private RoleRepository roleRepository;
-
-	public BlogApplication(RoleRepository roleRepository, PasswordEncoder passwordEncoder, UserRepository userRepository) {
-		this.roleRepository = roleRepository;
-		this.passwordEncoder = passwordEncoder;
-		this.userRepository = userRepository;
-	}
+	private final BootstrapData bootstrapData;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper() {
-		// Automates the mapping between Java bean types based on a convention over configuration approach.
-		return new ModelMapper();
-	}
-
 	@Override
 	public void run(String... args) throws Exception {
-		if (this.roleRepository.count() != 0) {
-			return;
-		}
-
-		// Create user role
-		Role roleUser = new Role();
-		roleUser.setName("ROLE_USER");
-		this.roleRepository.save(roleUser);
-
-		// Create admin role
-		Role roleAdmin = new Role();
-		roleAdmin.setName("ROLE_ADMIN");
-		this.roleRepository.save(roleAdmin);
+		bootstrapData.bootstrap();
 	}
 }
---
+++ src/main/java/com/blog/app/config/BootstrapData.java
@@ -0,0 +1,50 @@
+package com.blog.app.config;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.blog.app.entities.Role;
+import com.blog.app.entities.User;
+import com.blog.app.repositories.RoleRepository;
+import com.blog.app.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class BootstrapData {
+	private final PasswordEncoder passwordEncoder;
+	private final UserRepository userRepository;
+	private final RoleRepository roleRepository;
+
+	public void bootstrap() {
+		if (roleRepository.count() != 0) {
+			return;
+		}
+
+		Role roleUser = new Role();
+		roleUser.setName("ROLE_USER");
+
+		Role roleAdmin = new Role();
+		roleAdmin.setName("ROLE_ADMIN");
+
+		roleRepository.saveAll(List.of(roleUser, roleAdmin));
+
+		User user = new User();
+		user.setUsername("user");
+		user.setPassword(passwordEncoder.encode("user"));
+		user.setEmail("user@user.com");
+		user.setRoles(Set.of(roleUser));
+
+		User admin = new User();
+		admin.setUsername("admin");
+		admin.setPassword(passwordEncoder.encode("admin"));
+		admin.setEmail("admin@admin.com");
+		admin.setRoles(Set.of(roleUser, roleAdmin));
+
+		userRepository.saveAll(List.of(user, admin));
+	}
+}
